4bb1630afa4ca62facf10d4675743c9f8fd13559
Northern <jlsivan1127@proton.me>
2025-09-09 21:30:43 +0000

    made main.java

---
+++ main.java
@@ -0,0 +1,19 @@
+public class main {
+  public static void main(String[] args) {
+    //Coding is for the fun! I really love this Shit man
+    int apples = 45;
+    int bananas = 35;
+    int time = 2039;
+    double appleCost = 2.3;
+    double bananaCost = 1.2;
+
+    // Check if there is more apples than bananas
+    if (apples > bananas) {
+        System.out.println("There are " + (apples - bananas) + " more apples than bananas");
+    } else {
+        System.out.println("There are " + (bananas - apples) + " more bananas than apples");
+    }
+
+    //KILLING MYSELF
+  }
+}
